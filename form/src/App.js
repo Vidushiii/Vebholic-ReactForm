f7723b03e7cf03ebbc160bddec01649c0d1e5a78
Vidushiii <vidutomar19@gmail.com>
2021-03-13 00:53:32 +0530

    first commit

---
+++ form/src/App.js
@@ -0,0 +1,35 @@
+//import logo from './logo.svg';
+//import './App.css';
+import dummy from './dummy_payload.json';
+
+function App() {
+  return (
+    <div className="App container">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          </input>    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
