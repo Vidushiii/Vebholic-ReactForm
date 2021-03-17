412b907ff7ecca2a0ca57bf554e24983bc63e725
Vidushiii <vidutomar19@gmail.com>
2021-03-17 22:37:10 +0530

    commit

--- form/src/components/element.js
+++ form/src/components/element.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import Input from './elements/input';
+import DropDown from './elements/dropdown';
 
-const Element
+const Element = ({ field: { type, fieldName } }) => {
+    switch (type) {
+        case 'text':
+            return (<Input />)
+        case 'select':
+            return (<dropDown />)
+        default:
+            return null;
+    }
+}
+
+export default Element;
